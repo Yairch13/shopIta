019684d7b0583213ab53b23dc3ef96d8c81041b8
Yairch13 <yairchavez136va@gmail.com>
2022-12-06 14:33:06 -0600

    modificaciones json

--- js/accesorios.js
+++ js/accesorios.js
@@ -13,13 +13,22 @@ function cel(){
             let datos = JSON.parse(this.responseText);
             console.log(datos)
 
-            let res = document.querySelector('#res');
+            let res = document.querySelector('#respuesta');
             res.innerHTML = '';
 
 
             for (let item of datos) {
                 //console.log(item.precio);
-             if(item.id==1){ res.innerHTML += "Id:"+item.id+"<br><br>Nombre:"+item.nombre+"<br><br>Cantidad:"+item.cantidad+"<br><br>Descripcion:"+item.descripción+"<br><br>Precio:"+item.precio+"<br>"
+             if(item.id==1){ res.innerHTML += "<td>Id</td><td>"+item.id+"</td><tr><td>Nombre</td><td> "
+             +item.nombre+"</td><tr><td>Cantidad</td><td>"+item.cantidad+"</td><tr><td>Descripcion</td><td>"
+             +item.descripción+"</td><tr><td>Precio</td><td>"+item.precio+
+             "</td><tr><td>Descripción del teclado</td><td>"+item.descripcióndos+	
+             "</td><tr><td>Usos recomendados para producto</td><td>"+item.usos+
+             "</td><tr><td>Características especial</td><td>"+item.car+
+             "</td><tr><td>Número de teclas</td><td>"+item.num+
+             "</td><tr><td>Soporte de color de retroiluminación del teclado</td><td>"+item.sop+
+             "</td><tr><td>Estilo</td><td>"+item.est+
+            " </td><tr><td >Dimensiones</td><td >"+item.dim
                 
             }
                
